e0314e6f4137479cda932a5ede02d7a3eda31463
84danie <danielleholzberger@gmail.com>
2017-10-08 20:43:18 -0700

    Added Question and Student interfaces, IVoteService class, and driver class

---
+++ src/IVoteService.java
@@ -0,0 +1,8 @@
+
+public class IVoteService {
+
+	public IVoteService() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/SimulationDriver.java
@@ -0,0 +1,8 @@
+
+public class SimulationDriver {
+
+	public SimulationDriver() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
